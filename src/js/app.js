e141f5c9b50d4f873e441e31fccbc5667faa6291
Tomasz Tomczykiewicz <tomaszek92@gmail.com>
2018-05-26 16:41:31 +0200

    Prepare project

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// JS
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+import 'popper.js';
+import 'bootstrap';
+
+console.log('running');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader?presets[]=es2015'
+            },
+            {
+                test: /\.css$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(svg|gif|png|eot|woff|ttf)$/,
+                loaders: [
+                    'url-loader'
+                ]
+            }
+        ]
+    }
+}
